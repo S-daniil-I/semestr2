4e97266ac2b55a05781c34538ffb579b6b9c4213
Daniil <150480445+S-daniil-I@users.noreply.github.com>
2024-05-27 23:17:56 +0300

    Update laba 4.5.cpp(no friend func)

--- laba 4.5.cpp
+++ laba 4.5.cpp
@@ -1,16 +1,17 @@
-﻿#include "Vector.h"
+#include "Vector.h"
 #include <iostream>
 #include <vector>
 #include <time.h>
 #include <math.h>
 
 void maxDiff(Vector& vec1, Vector& vec2, Vector& FinalVec);
+Vector operator *(Vector& vector1, Vector& vector2);
 
 int main()
 {
     setlocale(LC_ALL, "RU");
     srand(time(NULL));
-    int choice_,size=5;
+    int choice_, size = 5;
     Vector vec;
     Vector vec1;
     Vector vec2;
@@ -42,31 +43,31 @@ int main()
     vec2.outVector();
     cout << endl;
     int choice = 0;
-        cout << " Выберите операцию: \n 1 : * \n 2 : / \n 3 : + \n 4 : -" << endl;
-        cin >> choice;
-        switch (choice)
-        {
-        case 1:
-            vec1.multiWithScalar(2);
-            vec2.multiWithScalar(3);
-            break;
-    
-        case 2:
-            vec1.divWithScalar(2);
-            vec2.divWithScalar(3);
-            break;
-        case 3:
-            vec1.sumWithScalar(2);
-            vec2.sumWithScalar(3);
-            break;
-        case 4:
-            vec1.subWithScalar(2);
-            vec2.subWithScalar(3);
-            break;
-        default:
-            cout << "Некорректный выбор!";
-            
-        }
+    cout << " Выберите операцию: \n 1 : * \n 2 : / \n 3 : + \n 4 : -" << endl;
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+        vec1.multiWithScalar(2);
+        vec2.multiWithScalar(3);
+        break;
+
+    case 2:
+        vec1.divWithScalar(2);
+        vec2.divWithScalar(3);
+        break;
+    case 3:
+        vec1.sumWithScalar(2);
+        vec2.sumWithScalar(3);
+        break;
+    case 4:
+        vec1.subWithScalar(2);
+        vec2.subWithScalar(3);
+        break;
+    default:
+        cout << "Некорректный выбор!";
+
+    }
     cout << "Вектор 1 после преобразования с 2: ";
     vec1.outVector();
     cout << endl;
@@ -77,27 +78,27 @@ int main()
     cout << "Вектор FinalVec после выполнения функции Func: ";
     FinalVec.outVector();
     cout << endl;
-    cout << "Работа с первым вектором"<<endl;
-        int choice1 = 0;
-        cout << " Выберите операцию: \n 1 : * \n 2 : / \n 3 : + \n 4 : -" << endl;
-        cin >> choice1;
-        switch (choice1)
-        {
-        case 1:
-            vec1.multiWithOtherVector(vec2, FinalVec);
-            break;
-        case 2:
-            vec1.divWithOtherVector(vec2, FinalVec);
-            break;
-        case 3:
-            vec1.sumWithOtherVector(vec2, FinalVec);
-            break;
-        case 4:
-            vec1.subWithOtherVector(vec2, FinalVec);
-            break;
-        default:
-            cout << "Некорректный выбор!";
-        }
+    cout << "Вызов функции:" << endl;
+    /*int choice1 = 0;
+    cout << " Выберите операцию: \n 1 : * \n 2 : / \n 3 : + \n 4 : -" << endl;
+    cin >> choice1;
+    switch (choice1)
+    {
+    case 1:
+        vec1.multiWithOtherVector(vec2, FinalVec);
+        break;
+    case 2:
+        vec1.divWithOtherVector(vec2, FinalVec);
+        break;
+    case 3:
+        vec1.sumWithOtherVector(vec2, FinalVec);
+        break;
+    case 4:
+        vec1.subWithOtherVector(vec2, FinalVec);
+        break;
+    default:
+        cout << "Некорректный выбор!";
+    }*/
     /*cout << "Преобразования первого вектора и второго: ";
     FinalVec.outVector();
     cout << endl;
@@ -109,7 +110,7 @@ int main()
     vec1.outVector();
     cout << endl;*/
     cout << "Результат умножения: ";
-    Vector result=vec1 * vec2;
+    Vector result = vec1 * vec2;
     result.outVector();
     return 0;
 }
@@ -142,12 +143,14 @@ void maxDiff(Vector& vec1, Vector& vec2, Vector& FinalVec)
         FinalVec.setVectorElement(i, sum);
     }
 }
-Vector operator * (const Vector& vec1, const Vector& vec2)
+Vector operator * (Vector& vector1, Vector& vector2)
 {
-    Vector result(vec1.size);
-    for (int i = 0; i < vec1.size; i++)
+
+    for (int i = 0; i < vector1.getVectorSize(); i++)
     {
-        result.array[i] = vec1.array[i] * vec2.array[i];
+        int temp;
+        temp = vector1.getVectorElement(i) * vector2.getVectorElement(i);
+        vector1.setVectorElement(i, temp);
     }
-    return result;
+    return vector1;
 }
